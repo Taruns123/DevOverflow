9f6a04e5042e8021f2c94755041c824fc31bf2f0
Tarun Shetty <85640802+Taruns123@users.noreply.github.com>
2024-06-23 10:45:17 +0530

    updated the layout and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,56 @@
 import React from 'react'
 import {
   ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
 } from '@clerk/nextjs'
+
 import './globals.css'
+
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from 'next/font/google'
+import type { Metadata } from 'next'
+
+const inter = Inter(
+  {
+    subsets: ['latin'],
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    variable: '--font-inter',
+  }
+)
+
+const spaceGrotesk = Space_Grotesk(
+  {
+    subsets: ['latin'],
+    weight: ['300', '400', '500', '600', '700'],
+    variable: '--font-spaceGrotesk',
+  }
+)
+
+
+export const metaData: Metadata = {
+  title: 'DevFlow',
+  description: 'A communtiy for developers to share their knowledge and grow together',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  }
+}
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500'
+        }
+      }}
+    >
       <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+        <body className={`${inter.variable} ${spaceGrotesk.variable} `}>
+
           {children}
         </body>
       </html>
