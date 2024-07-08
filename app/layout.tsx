658bf525df7a79a977e7b9f70a8e35ec3d5fa415
Tarun Shetty <85640802+Taruns123@users.noreply.github.com>
2024-07-09 00:07:17 +0530

    some more changes for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import './globals.css'
 
 // eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from 'next/font/google'
-import type { Metadata } from 'next'
+// import type { Metadata } from 'next'
 import { ThemeProvider } from '@/context/ThemeProvider'
 
 const inter = Inter(
@@ -27,13 +27,13 @@ const spaceGrotesk = Space_Grotesk(
 )
 
 
-export const metaData: Metadata = {
-  title: 'DevFlow',
-  description: 'A communtiy for developers to share their knowledge and grow together',
-  icons: {
-    icon: '/assets/images/site-logo.svg',
-  }
-}
+// export const metaData: Metadata = {
+//   title: 'DevFlow',
+//   description: 'A communtiy for developers to share their knowledge and grow together',
+//   icons: {
+//     icon: '/assets/images/site-logo.svg',
+//   }
+// }
 
 export default function RootLayout({
   children,
